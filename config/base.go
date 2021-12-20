d9fd7d2ab75c9c4e5d1edd9c71f4eeab3298c3d0
REZA FAJRIANSYAH <rezafajriansyah21@gmail.com>
2021-12-20 19:17:18 +0700

    [FEATURE] Connect with postgres

--- config/base.go
+++ config/base.go
@@ -1,30 +1,64 @@
 package config
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"main/controllers"
-	"net/url"
 	"os"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
+	// _ "github.com/jackc/pgx/stdlib"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	// _ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/spf13/viper"
 )
 
 const MODE = "release"
 
 type Server struct {
-	DB	*sql.DB
+	DB	*sqlx.DB
 	Router *gin.Engine
+}
+
+type DbConn struct {
+	Db *gorm.DB
 }
 
 func GetEnvWithKey(key string) string {
 	return os.Getenv(key)
 }
 
-func ConnectDB() (*sql.DB, error) {
+func NewDbConn() *DbConn {
+	dbHost := "localhost"
+	dbPort := "5432"
+	dbUser := "reza"
+	dbPassword := "1234"
+	dbName := "toko_buku"
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", dbHost, dbUser, dbPassword, dbName, dbPort)
+
+	env := "dev"
+	//Daftar konfigurasi GORM apa saja yang ada
+	//https://gorm.io/docs/gorm_config.html
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if env == "dev" {
+		return &DbConn{
+			Db: db.Debug(),
+		}
+	}
+
+	return &DbConn{
+		Db: db,
+	}
+
+}
+
+func ConnectDB() (*sqlx.DB, error) {
 	// get := GetEnvWithKey
 	// DB_USER := get("DB_USER")
 	// DB_PASS := get("DB_PASS")
@@ -37,15 +71,15 @@ func ConnectDB() (*sql.DB, error) {
 	DB_HOST := viper.GetString("database.DB_HOST")
 	DB_PORT := viper.GetString("database.DB_PORT")
 	DB_NAME := viper.GetString("database.DB_NAME")
-	DB_LOC := viper.GetString("database.DB_LOC")
+	// DB_LOC := viper.GetString("database.DB_LOC")
 	//String format untuk koneksi
-	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
-	val := url.Values{}
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", DB_HOST, DB_USER, DB_NAME, DB_PASS, DB_PORT)
+	// val := url.Values{}
 	// menambahkan value location
-	val.Add("loc", DB_LOC)
-	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+	// val.Add("loc", DB_LOC)
+	// dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
 	// Buka koneksi database
-	db, err := sql.Open(`mysql`, dsn)
+	db, err := sqlx.Connect(`pgx`, dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -55,8 +89,15 @@ func ConnectDB() (*sql.DB, error) {
 		_, _ = fmt.Scanln()
 		log.Fatal(err)
 	}
-	log.Println("DataBase Successfully Connected")
+	log.Println("Database Successfully Connected")
 	return db, err
+}
+
+func (d *DbConn) Migration(tables ...interface{}) {
+	err := d.Db.AutoMigrate(tables...)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func CreateRouter() *gin.Engine {
@@ -73,7 +114,7 @@ func CreateRouter() *gin.Engine {
 	return r
 }
 
-func InitRouter(db *sql.DB, r *gin.Engine) *Server {
+func InitRouter(db *sqlx.DB, r *gin.Engine) *Server {
 	return &Server{
 		DB: db,
 		Router: r,
